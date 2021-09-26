77399433e71331f9dea433e965cfa458b710e89d
Ahmad Feroz <88190058+akhmadferoz-maqsad@users.noreply.github.com>
2021-09-26 19:40:19 +0500

    [FEAT] Completed implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
-public class Main {
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main{
 
     public static void main(String[] args) {
 	// write your code here
+        TreeMap<Seat, Passenger> passengerList = new TreeMap<>();
+
+        passengerList.put( new Seat("17D"), new Passenger("Asad", "AQ98DF") );
+        passengerList.put( new Seat("02B"), new Passenger("Faisal", "JMUILP") );
+        passengerList.put( new Seat("15B"), new Passenger("Farooq", "78KLPX") );
+        passengerList.put( new Seat("32A"), new Passenger("Ahsan", "OLSIVF") );
+        passengerList.put(new Seat("18F"), new Passenger("Faiza", "UJ89IK") );
+        passengerList.put( new Seat("20C"), new Passenger("Amna", "UXDFW0") );
+
+        for (Map.Entry<Seat, Passenger> entry : passengerList.entrySet()) {
+            System.out.println( entry.getValue() + ", " + entry.getKey() );
+        }
     }
 }
--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -2,4 +2,19 @@ package com.company;
 
 public class Passenger {
     private String name, PNR;
+
+    Passenger() {
+        this.name = "Passenger not found";
+        this.PNR = "PNR not found";
+    }
+
+    Passenger(String n, String bookingNumber) {
+        this.name = n;
+        this.PNR = bookingNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PNR: " + PNR + ", Name: " + name;
+    }
 }
